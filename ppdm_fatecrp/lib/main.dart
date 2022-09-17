3298c1f9175b940dee54e4fbc86ce4ae92b18c69
WallisonRP <wallison0076@gmail.com>
2022-09-17 19:11:38 -0300

    created turma

--- ppdm_fatecrp/lib/main.dart
+++ ppdm_fatecrp/lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:ppdm_fatecrp/view/tela_cadastro.dart';
 import 'package:ppdm_fatecrp/view/tela_de_login.dart';
 import 'package:ppdm_fatecrp/view/menu_tela_inicial.dart';
+import 'package:ppdm_fatecrp/view/turma.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: MenuTelaInicial(),
+    home: Turma(),
     debugShowCheckedModeBanner: false,
   ));
 }
---
+++ ppdm_fatecrp/lib/view/turma.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class Turma extends StatefulWidget {
+  const Turma({super.key});
+
+  @override
+  State<Turma> createState() => _TurmaState();
+}
+
+class _TurmaState extends State<Turma> {
+  List lista = [
+    'Camila Silva',
+    'Ciro Abib',
+    'Fabio Chubinho',
+    'Gabriel Oliveira',
+    'Guilherme Crisóstopo',
+    'Guilherme Lima Beta',
+    'Gustavo Latrocínio',
+    'Gustavo Macrino',
+    'Juliano Henrico',
+    'Maria Duda',
+    'Pedro Brilhadori',
+    'Raul Benado',
+    'Wallison Pereira'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffD9D9D9),
+        title: Text(
+          '4° Semestre',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 10, 0, 10),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                    width: 1.0, color: Color.fromARGB(255, 224, 224, 224)),
+              ),
+            ),
+            child: Row(
+              children: [
+                Text("Ordem alfabética"),
+                Icon(Icons.keyboard_arrow_down)
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: lista.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: ListTile(
+                    minVerticalPadding: 20,
+                    title: Text(lista[index]),
+                    subtitle: Text('(16) 98765-4321'),
+                    leading: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xffD9D9D9)),
+                      width: 60,
+                      height: 70,
+                    ),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.keyboard_arrow_right,
+                          color: Colors.black,
+                          size: 28,
+                        )
+                      ],
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                          width: 1.0,
+                          color: Color.fromARGB(255, 224, 224, 224)),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
