dbc4eb6a43e162fd97c4b572dee6087033e36cdb
WallisonRP <wallison0076@gmail.com>
2022-09-19 12:31:18 -0300

    created tela_sobre

--- ppdm_fatecrp/lib/main.dart
+++ ppdm_fatecrp/lib/main.dart
@@ -7,11 +7,12 @@ import 'package:ppdm_fatecrp/view/tela_de_login.dart';
 import 'package:ppdm_fatecrp/view/menu_tela_inicial.dart';
 import 'package:ppdm_fatecrp/view/tela_editar_perfil.dart';
 import 'package:ppdm_fatecrp/view/tela_perfil.dart';
+import 'package:ppdm_fatecrp/view/tela_sobre.dart';
 import 'package:ppdm_fatecrp/view/turma.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: TelaCadastrarTurma(),
+    home: TelaSobre(),
     debugShowCheckedModeBanner: false,
   ));
 }
---
+++ ppdm_fatecrp/lib/view/tela_sobre.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+import '../model/card_itens.dart';
+
+class TelaSobre extends StatefulWidget {
+  const TelaSobre({super.key});
+
+  @override
+  State<TelaSobre> createState() => _TelaSobreState();
+}
+
+class _TelaSobreState extends State<TelaSobre> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sobre'),
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(16, 32, 16, 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Informações do projeto',
+              style: TextStyle(
+                fontSize: 20,fontWeight: FontWeight.bold
+              ),
+              ),
+            SizedBox(height: 20,),
+            Flexible(child: Text('Aqui você encontra tudo sobre a equipe responsável por este projeto, Aqui você encontra tudo sobre a equipe responsável por este projeto ...Aqui você encontra tudo sobre a equipe responsável por este projeto ...Aqui você encontra tudo sobre a equipe responsável por este projeto ...Aqui você encontra tudo sobre a equipe responsável por este projeto ...Aqui você encontra tudo sobre a equipe responsável por este projeto ...Aqui você encontra tudo sobre a equipe responsável por este dsaa')),
+            SizedBox(height: 35,),
+            Text(
+              'Desenvolvedores',
+              style: TextStyle(
+                fontSize: 20,fontWeight: FontWeight.bold
+              )
+              ),
+            SizedBox(height: 15,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Color.fromARGB(255, 224, 224, 224)
+                  ),
+                  margin: EdgeInsets.only(right: 16),
+                  width: 80,
+                  height: 80,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Rodrigo de Oliveira Plotze',
+                      style: TextStyle(
+                fontSize: 16,fontWeight: FontWeight.w600
+              )
+                      ),
+                    SizedBox(height: 4,),
+                    Text('Orientador')
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 18,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Color.fromARGB(255, 224, 224, 224)
+                  ),
+                  margin: EdgeInsets.only(right: 16),
+                  width: 80,
+                  height: 80,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Gabriel Afonso Pinho de Oliveira',
+                      style: TextStyle(
+                fontSize: 16,fontWeight: FontWeight.w600
+              )
+                      ),
+                    SizedBox(height: 4,),
+                    Text('Desenvolvedor')
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 18,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Color.fromARGB(255, 224, 224, 224)
+                  ),
+                  margin: EdgeInsets.only(right: 16),
+                  width: 80,
+                  height: 80,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Wallison Franklin Pereira',
+                      style: TextStyle(
+                fontSize: 16,fontWeight: FontWeight.w600
+              )
+                      ),
+                    SizedBox(height: 4,),
+                    Text('Desenvolvedor')
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
