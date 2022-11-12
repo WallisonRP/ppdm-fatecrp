1d461bd1d59320320319b824ca0af52f4d24b51c
WallisonRP <wallison0076@gmail.com>
2022-11-12 17:05:10 -0300

    remoção de hint

--- lib/view/widgets/caixa_de_texto_redonda.dart
+++ lib/view/widgets/caixa_de_texto_redonda.dart
@@ -8,15 +8,15 @@ CaixaDeTextoRedonda(
     controller: controller,
     readOnly: leitura,
     decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.black)),
-        focusedBorder: OutlineInputBorder(
+      enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(color: Colors.black),
-        ),
-        labelStyle: TextStyle(color: Colors.black),
-        labelText: label,
-        hintText: 'teste'),
+          borderSide: BorderSide(color: Colors.black)),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Colors.black),
+      ),
+      labelStyle: TextStyle(color: Colors.black),
+      labelText: label,
+    ),
   );
 }
