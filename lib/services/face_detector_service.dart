9578b5fa6e36433242b02c12a48a2c275d275bec
WallisonRP <wallison0076@gmail.com>
2023-01-13 16:50:36 -0300

    adição da detecção de faces

---
+++ lib/services/face_detector_service.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:ppdm_fatecrp/services/camera_service.dart';
+import 'package:ppdm_fatecrp/services_locator.dart';
+
+class FaceDetectorService {
+  final CameraService _cameraService = locator<CameraService>();
+
+  late FaceDetector _faceDetector;
+  FaceDetector get faceDetector => _faceDetector;
+
+  List<Face> _faces = [];
+  List<Face> get faces => _faces;
+  bool get faceDetected => _faces.isNotEmpty;
+
+  void initialize() {
+    _faceDetector = FaceDetector(
+        options: FaceDetectorOptions(
+            performanceMode: FaceDetectorMode.fast,
+            enableContours: true,
+            enableClassification: true));
+  }
+
+  Future<void> detectFacesFromImage(CameraImage image) async {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (Plane plane in image.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize =
+        Size(image.width.toDouble(), image.height.toDouble());
+
+    InputImageRotation imageRotation =
+        _cameraService.cameraRotation ?? InputImageRotation.rotation0deg;
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation,
+      inputImageFormat: InputImageFormat.yuv420,
+      planeData: image.planes.map(
+        (Plane plane) {
+          return InputImagePlaneMetadata(
+            bytesPerRow: plane.bytesPerRow,
+            height: plane.height,
+            width: plane.width,
+          );
+        },
+      ).toList(),
+    );
+
+    InputImage facesInImage = InputImage.fromBytes(
+      bytes: bytes,
+      inputImageData: inputImageData,
+    );
+
+    _faces = await _faceDetector.processImage(facesInImage);
+  }
+
+  dispose() {
+    _faceDetector.close();
+  }
+}
--- lib/services_locator.dart
+++ lib/services_locator.dart
@@ -1,9 +1,12 @@
 import 'package:ppdm_fatecrp/services/camera_service.dart';
 
 import 'package:get_it/get_it.dart';
+import 'package:ppdm_fatecrp/services/face_detector_service.dart';
 
 final locator = GetIt.instance;
 
 void setupServices() {
   locator.registerLazySingleton<CameraService>(() => CameraService());
+  locator.registerLazySingleton<FaceDetectorService>(() => FaceDetectorService());
+
 }
