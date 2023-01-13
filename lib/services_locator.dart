cfe5fd6af8ed7ea40f573991ce25af1e5a8a37fe
WallisonRP <wallison0076@gmail.com>
2023-01-13 16:07:56 -0300

    Adição do pacote get it

---
+++ lib/services_locator.dart
@@ -0,0 +1,9 @@
+import 'package:ppdm_fatecrp/services/camera_service.dart';
+
+import 'package:get_it/get_it.dart';
+
+final locator = GetIt.instance;
+
+void setupServices() {
+  locator.registerLazySingleton<CameraService>(() => CameraService());
+}
